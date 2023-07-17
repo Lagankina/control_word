bd0d2ba307f0ae80a1f4ddbabbd9b25dd5fd9dde
Adel <reirianet@mail.ru>
2023-07-17 14:00:28 +0300

    Вывод основных значений

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// <!-- Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами. -->
+
+Console.Write("Введите длину массива: "); 
+int len = int.Parse(Console.ReadLine());
+string[] myArray = new string[len];
+
+GetEllementArray(myArray);
+Console.WriteLine("Исходный массив: ");
+PrintArray(myArray);
+Console.WriteLine();
+
+string[] resultArray = trim(myArray);
+
+
+
+
+
+
+
+
